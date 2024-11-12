836d73082f677ef8e838e5a219c9ed9bfc64ffaa
Anchorite <ai_anchorite@proton.me>
2024-11-13 04:21:55 +1300

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "",
   icon: "icon.png",
   menu: async (kernel, info) => {
-    let installed = info.exists("env")
+    let installed = info.exists("app/env")
     let running = {
       install: info.running("install.js"),
       start: info.running("start.js"),
