76c148e886a939a8e3e2272276d0424f42251b9e
6Morpheus6 <wanton-decay@gmx.net>
2025-04-29 01:05:11 +0200

    save disk space, script version 3.7

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  version: "2.0",
+  version: "3.7",
   title: "Clarity Refiners UI",
   description: "An enhanced local port of finegrain-image-enhancer powered by Refiners (https://huggingface.co/spaces/finegrain/finegrain-image-enhancer), which was adapted from philz1337x's Clarity Upscaler (https://github.com/philz1337x/clarity-upscaler)",
   icon: "icon.png",
@@ -11,7 +11,8 @@ module.exports = {
       start: info.running("start.js"),
       update: info.running("update.js"),
       reset: info.running("reset.js"),
-	  cache: info.running("delete-cache.js")
+      link: info.running("link.js"),
+	    cache: info.running("delete-cache.js")
     }
     if (running.install) {
       return [{
@@ -63,6 +64,13 @@ module.exports = {
           text: "Clearing Cache",
           href: "delete-cache.js",
         }]
+      } else if (running.link) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Deduplicating",
+          href: "link.js",
+        }]
       } else {
         return [{
           default: true,
@@ -81,11 +89,15 @@ module.exports = {
           icon: "fa-regular fa-circle-xmark",
           text: "Reset",
           href: "reset.js",
-		  confirm: "Are you sure you wish to reset this app?",
-	    }, {
+          confirm: "Are you sure you wish to reset this app?",
+        }, {
           icon: "fa-regular fa-circle-xmark",
           text: "Delete Gradio Image Cache",
           href: "delete-cache.js",
+        }, {
+          icon: "fa-solid fa-file-zipper",
+          text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
+          href: "link.js",
         }]
       }
     } else {
